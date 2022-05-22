cafc3b4471bd11a80bad20a04288d6fc368f3a27
Leandro.sarvi <leandromartinsarvi@hotmail.com>
2022-05-22 19:47:33 -0300

    slider autoplay

--- js/main.js
+++ js/main.js
@@ -51,3 +51,28 @@
     });   
 })(jQuery);
 
+/* SLIDER */
+const swiper = new Swiper('.swiper', {
+    autoplay: {
+        delay: 1500,
+      },
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
